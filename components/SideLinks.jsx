8c401e39a0e6ce11ede31e9659a7d6b1dad29391
kevinthomas18 <kevin@spiderworks.in>
2024-09-02 17:11:11 +0530

    add path to sidelinks

--- components/SideLinks.jsx
+++ components/SideLinks.jsx
@@ -1,12 +1,14 @@
-"use client";
 import React from "react";
 import { CiEdit } from "react-icons/ci";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FaImages } from "react-icons/fa";
 import { MdInsertChartOutlined } from "react-icons/md";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 const SideLinks = () => {
+  const params = useParams();
+  console.log(params, "side params");
   return (
     <div className="absolute flex flex-row space-y-2 top-40 left-10 md:flex-col md:space-x-2">
       <button className="text-gray-600 hover:text-orange-400 font-semibold flex items-center">
@@ -18,16 +20,19 @@ const SideLinks = () => {
         Delete
       </button>
       <Link
-        href="edit-images"
+        href={`/edit-blog/edit-images/${params.slug}`}
         className="text-gray-600 hover:text-blue-600 font-semibold flex items-center"
       >
         <FaImages className="mx-2" />
         Images
       </Link>
-      <button className="text-gray-600 hover:text-green-600 font-semibold flex items-center">
+      <Link
+        href={`/edit-blog/add-seo/${params.slug}`}
+        className="text-gray-600 hover:text-green-600 font-semibold flex items-center"
+      >
         <MdInsertChartOutlined className="mx-2" />
         SEO
-      </button>
+      </Link>
     </div>
   );
 };
